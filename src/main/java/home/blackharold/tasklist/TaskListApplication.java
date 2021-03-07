cf4aebb1d58d2014af7bbe8572b32ad4307990ca
Evgeniy Gul <gulevgeniy@gmail.com>
2021-03-07 18:22:42 +0300

    init commit

---
+++ src/main/java/home/blackharold/tasklist/TaskListApplication.java
@@ -0,0 +1,13 @@
+package home.blackharold.tasklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class TaskListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskListApplication.class, args);
+    }
+}
