345b94601ec902f3cc22a60c6f147e3136d49439
Evgeniy Gul <gulevgeniy@gmail.com>
2021-03-09 20:49:48 +0300

    postman testing collection

--- src/main/java/home/blackharold/tasklist/controller/CategoryController.java
+++ src/main/java/home/blackharold/tasklist/controller/CategoryController.java
@@ -1,12 +1,15 @@
 package home.blackharold.tasklist.controller;
 
 import home.blackharold.tasklist.entity.Category;
+import home.blackharold.tasklist.entity.Category;
+import home.blackharold.tasklist.entity.Priority;
 import home.blackharold.tasklist.repo.CategoryRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/category")
@@ -21,7 +24,37 @@ public class CategoryController {
 
     @GetMapping("/list")
     public List<Category> test() {
-
         return categoryRepository.findAll();
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Category> add(@RequestBody Category category) {
+        if (category.getId() != null && category.getId() != 0) {
+            return new ResponseEntity("redutant param: id must be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        if (category.getTitle() == null) {
+            return new ResponseEntity("missed param 'title'", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(categoryRepository.save(category));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity update(@RequestBody Category category) {
+        if (category.getId() == null && category.getId() == 0) {
+            return new ResponseEntity("missed param: id must be not null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(categoryRepository.save(category));
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Category> getById(@PathVariable Long id) {
+        Category category;
+        try {
+            category = categoryRepository.findById(id).get();
+        } catch (NoSuchElementException nse) {
+            return new ResponseEntity("No such element", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(category);
     }
 }
--- src/main/java/home/blackharold/tasklist/controller/PriorityController.java
+++ src/main/java/home/blackharold/tasklist/controller/PriorityController.java
@@ -3,11 +3,12 @@ package home.blackharold.tasklist.controller;
 import home.blackharold.tasklist.entity.Priority;
 import home.blackharold.tasklist.repo.PriorityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/priority")
@@ -25,4 +26,46 @@ public class PriorityController {
         return priorityRepository.findAll();
     }
 
+    @PostMapping("/add")
+    public ResponseEntity<Priority> add(@RequestBody Priority priority) {
+        if (priority.getId() != null && priority.getId() != 0) {
+            return new ResponseEntity("redutant param: id must be null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        if (priority.getTitle() == null || priority.getColor() == null) {
+            return new ResponseEntity("missed param 'title' or 'color'", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return ResponseEntity.ok(priorityRepository.save(priority));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Priority> update(@RequestBody Priority priority) {
+        System.out.println("priority id: " + priority.getId());
+        if (priority.getId() == null && priority.getId() == 0) {
+            return new ResponseEntity("missed param: id must be not null", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(priorityRepository.save(priority));
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Priority> getById(@PathVariable Long id) {
+        Priority priority;
+        try {
+            priority = priorityRepository.findById(id).get();
+        } catch (NoSuchElementException nse) {
+            return new ResponseEntity("No such element: " + id, HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(priority);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteById(@PathVariable Long id) {
+        try {
+            priorityRepository.deleteById(id);
+        } catch (IllegalArgumentException iae) {
+            return new ResponseEntity("wrong id: " + id, HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok("object with id: " + id + " deleted");
+    }
 }
